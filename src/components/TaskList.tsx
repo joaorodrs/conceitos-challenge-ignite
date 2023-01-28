3396038867250df81dddf4445b85f11b3dedc9b3
João Paulo <joao.paulo@inovan.do>
2023-01-28 08:35:24 -0300

    Initial commit

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,27 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return
+
+    setTasks(prev => [...prev, { id: Math.random(), title: newTaskTitle, isComplete: false }])
+    setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const currentTask = tasks.find(task => task.id === id)
+
+    if (!currentTask) return
+
+    setTasks(prev => [
+      ...prev.filter(task => task.id !== id),
+      { ...currentTask, isComplete: true }
+    ])
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(prev => prev.filter(task => task.id !== id))
   }
 
   return (
